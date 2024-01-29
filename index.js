097b9315d02ab7e6c6f20760c556acf7a0bcf673
Andrew Defee <andrew@andrewdefee.com>
2024-01-29 02:58:59 -0600

    Adds full report exports, includes more playerData
    - Appearance data & various totals now included
    - Supports report exports
    - `gameSaveDirectoryPath` should now be the only required config option, app should gracefully handle other defaults.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@
 const fs = require('fs');
 const { promisify } = require('util');
 const { exec } = require('child_process');
+const path = require('path');
 const execAsync = promisify(exec);
 require('dotenv').config()
 var argv = require('minimist')(process.argv.slice(2));
@@ -12,34 +13,59 @@ const cloneGitRepo = require('./lib/cloneGitRepo');
 const getAppConfig = require('./lib/getAppConfig');
 const deleteFileSync = require('./lib/deleteFile');
 const convertSavAndJson = require('./lib/convertSavAndJson');
+const filenameCompatibleTimestamp = require('./lib/filenameCompatibleTimestamp');
+
+const attributeToCheahJs = 'Thanks much, @cheajs!';
+const successfulConversion = `${attributeToCheahJs} Looks like conversion was successful.`;
 
 // Main saveEditor
 async function saveEditorMain() {
   const errors = [];
+  const appStartedTimestamp = new Date().toISOString();
+  const reportData = {
+    summary: {},
+    players: [],
+  };
 
   if (!argv || !argv['c']) {
     console.info("Using default config.json path of './config.json'. If you want to use a different path, use the '-c' flag");
   }
 
-  const appConfig = getAppConfig(argv && argv['c'] ? argv['c'] : './config.json');
+  let appConfig = getAppConfig(argv && argv['c'] ? argv['c'] : './config.json');
 
   if (!appConfig) {
-    errors.push('Unable to load app config, so unable to continue.');
-    console.error('Local "config.json" file is missing. Please create it - see README or "config.example.json" for details and examples.');
+    errors.push('Unable to load app config, so cannot continue');
+    console.err('Unable to load app config, so cannot continue. See README.md to learn about config.json and how to create it.')
+    return;
+  }
+
+  if (!appConfig?.gameSaveDirectoryPath || appConfig?.gameSaveDirectoryPath?.length < 1) {
+    errors.push('Unable to find gameSaveDirectoryPath in config.json, so we cannot continue.');
+    console.error('The only required field in config.json is gameSaveDirectoryPath, which should be the path to a valid Palworld save directory. This field is missing. See README.md for more info.');
     return;
   }
 
   // Setup placeholders for converted save data.
   let levelSavJson = null; // This will be the converted Level.sav file, if it exists.
   let levelSavMetaJson = null; // This will be the converted LevelMeta.sav file, if it exists.
-  let playerSavJson = null; // This will be all converted Player.sav files (nested together) that exist.
+  let refinedPlayerMap = []; // This will be a refined version of the Players list that we assemble
+
+  const {
+    gameSaveDirectoryPath,
+    savToJsonConversion = {
+      enable: true,
+      relativeInstallPath: './palworld-save-tools-cheahjs',
+      repoUrl: 'https://github.com/cheahjs/palworld-save-tools.git',
+      convertFreshEveryRun: true,
+    },
+  } = appConfig;
 
   const {
     enable: enableSavJsonConversion,
     relativeInstallPath: savJsonRelativeInstallPath,
     repoUrl: savJsonRepoUrl,
     convertFreshEveryRun,
-  } = appConfig.savToJsonConversion;
+  } = savToJsonConversion;
 
   try {
     if (!fileExists(`${savJsonRelativeInstallPath}/convert.py`) && enableSavJsonConversion) {
@@ -74,10 +100,6 @@ async function saveEditorMain() {
 
   if (errors.length < 1) {
     try {
-      const {
-        gameSaveDirectoryPath
-      } = appConfig;
-
       console.info(`Checking for save files in ${gameSaveDirectoryPath}...`)
 
       // First, check for Level.sav
@@ -114,7 +136,7 @@ async function saveEditorMain() {
         if (runConversion) {
           const [isLevelConversionSuccessful, levelConversionErrors] = await convertSavAndJson(savJsonRelativeInstallPath, iterativePath, iterativeLabel);
           if (isLevelConversionSuccessful) {
-            console.info(`Thanks, CheahJS! Looks like conversion was successful.`);
+            console.info(`${successfulConversion}`);
           } else {
             errors.push(...levelConversionErrors);
           }
@@ -162,7 +184,7 @@ async function saveEditorMain() {
         if (runConversion) {
           const [isLevelMetaConversionSuccessful, levelMetaConversionErrors] = await convertSavAndJson(savJsonRelativeInstallPath, iterativePath, iterativeLabel);
           if (isLevelMetaConversionSuccessful) {
-            console.info(`Thanks, CheahJS! Looks like conversion was successful.`);
+            console.info(`${successfulConversion}`);
           } else {
             errors.push(...levelMetaConversionErrors);
           }
@@ -207,26 +229,265 @@ async function saveEditorMain() {
     // This should be an array of objects, each of which contains some general player data like level, handle, etc.
     const worldPlayerData = levelSavJson.properties.worldSaveData.value.CharacterSaveParameterMap.value;
 
-    const quickStats = {
+    reportData.summary = {
       magic,
       saveGameVersion: save_game_version,
       engineVersion: `${engine_version_major}.${engine_version_minor}.${engine_version_patch} (${engine_version_branch})`,
       countTotalPlayers: worldPlayerData?.length || 'Could not find player data'
     }
     
-    console.info(`Here's a quick summary of your world save:`, quickStats);
+    console.info('\n\n===========================')
+    console.info(`Here's a quick summary of your world save:`, reportData.summary);
+    console.info('===========================\n\n')
 
-    // Now let's grab all of requested player changes and 
+    /**
+     * Before we continue, we can do some validation.
+     * For exmaple, we'd expect to find one save file per player entry.
+     */
+    if (worldPlayerData.length < 1) {
+      errors.push('No player data found in Level.sav, so we cannot continue.');
+    }
+
+    if (errors.length < 1) {
+      let iteratedPlayerIndex = 0;
+      for (const singleRawPlayer of worldPlayerData) {
+        let refinedSinglePlayer = {};
+        const nestedPlayerData = singleRawPlayer?.value?.RawData?.value?.object?.SaveParameter?.value;
+        refinedSinglePlayer = {
+          guid: singleRawPlayer?.key?.PlayerUId?.value || 'N/A',
+        }
+
+        // Determine expected save file name based on GUID
+        if (refinedSinglePlayer.guid !== 'N/A') {
+          refinedSinglePlayer.expectedSaveFileName = `${refinedSinglePlayer.guid?.replace(/-/g, '').toUpperCase()}.sav`;
+        } else {
+          errors.push(`The player at index ${iteratedPlayerIndex} does not have a valid GUID, so we cannot determine their save file name. This may indicate an incomplete or corrupted player record. No modifications will attempt to be made to this player!`)
+        }
+
+        if (nestedPlayerData) {
+          // Default to level 1 if we can't find it. This entry may not exist if we haven't dinged yet.
+          refinedSinglePlayer.level = nestedPlayerData?.Level?.value || 1; 
+          // Default to 0 if we can't find it. This entry may not exist if we haven't earned any xp yet (fresh install)
+          refinedSinglePlayer.exp = nestedPlayerData?.Exp?.value || 0;
+          // This is our nickname, etc. Case-sensitive.
+          refinedSinglePlayer.handle = nestedPlayerData?.NickName?.value || null;
+          // Yes, the value.value.value, etc is intentional. These are heavily nested.
+          refinedSinglePlayer.currentHp = nestedPlayerData?.HP?.value?.Value?.value || null;
+          refinedSinglePlayer.maxHp = nestedPlayerData?.MaxHP?.value?.Value?.value || null;
+          // Not as nested as the above.
+          refinedSinglePlayer.hunger = nestedPlayerData?.FullStomach?.value || null;
+          
+          /**
+           * Attempt to load the save file
+           */
+          try {
+            if (refinedSinglePlayer.expectedSaveFileName) {
+              let runPlayerSaveFileConversion = false;
+              const thisPlayerSaveFilePath = `${gameSaveDirectoryPath}/Players/${refinedSinglePlayer.expectedSaveFileName}`;
+              if (fileExists(`${thisPlayerSaveFilePath}.json`)) {
+                console.info(`Found existing JSON version of ${refinedSinglePlayer.handle}'s save file.`);
+                if (convertFreshEveryRun) {              
+                  // Wipe existing Players/<GUID>.json
+                  deleteFileSync(`${thisPlayerSaveFilePath}.json`);
+                  runPlayerSaveFileConversion = true;
+                }
+              } else {
+                console.info(`Looks like we need to convert ${refinedSinglePlayer.handle}'s save file to JSON.`);
+                runPlayerSaveFileConversion = true;
+              }
+
+              if (runPlayerSaveFileConversion) {
+                const [isPlayerConversionSuccessful, playerConversionErrors] = await convertSavAndJson(savJsonRelativeInstallPath, thisPlayerSaveFilePath, `${refinedSinglePlayer.handle || refinedSinglePlayer.guid}`);
+                if (isPlayerConversionSuccessful) {
+                  console.info(`${successfulConversion}`);
+                } else {
+                  errors.push(...playerConversionErrors);
+                }
+              } else {
+                console.info(`Looks like we already have a JSON version of ${refinedSinglePlayer.handle}'s save file, so we'll use that.`);
+              }
+
+              try {
+                const thisPlayerSavRawJsonData = fs.readFileSync(`${thisPlayerSaveFilePath}.json`);
+                const thisPlayerSavParsedJsonData = JSON.parse(thisPlayerSavRawJsonData);
+                if (thisPlayerSavParsedJsonData) {
+                  
+                  /**
+                   * Player file has some stuff the Level.sav doesn't have.
+                   * For example, TechnologyPoints, UnlockedRecipeTechnologyNames, and PlayerCharacterMakeData (appearance) data.
+                   * There's some overlap, though - like the voiceID, which is in both.
+                   */
+                  const playerFileNestedData = thisPlayerSavParsedJsonData?.properties?.SaveData?.value || null;
+                  if (!playerFileNestedData) {
+                    errors.push(`Unable to find expected data (tech points, appearance, etc) for player with GUID: "${refinedSinglePlayer.guid}". This may indicate an incomplete or corrupted player record.`);
+                  } else {
+                    console.info("Found player save data, loading...");
+
+                    // This object may not exist if player hasn't levelled yet, so default to 0
+                    refinedSinglePlayer.totalTechnologyPoints = playerFileNestedData?.TechnologyPoint?.value || 0;
+                    refinedSinglePlayer.totalAncientTechnologyPoints = playerFileNestedData?.bossTechnologyPoint?.value || 0;
+
+                    if (appConfig?.reporting?.showPlayerUnlockedRecipes !== false) {
+                      refinedSinglePlayer.unlockedRecipes = playerFileNestedData?.UnlockedRecipeTechnologyNames?.value?.values ? playerFileNestedData?.UnlockedRecipeTechnologyNames?.value?.values : [];
+                    }
+
+                    if (appConfig?.reporting?.showPlayerAppearance !== false) {
+                      if (!playerFileNestedData?.PlayerCharacterMakeData?.value) {
+                        refinedSinglePlayer.appearance = {
+                          error: 'Unable to find PlayerCharacterMakeData for this player.'
+                        };
+                        errors.push(`Unable to find PlayerCharacterMakeData for player with GUID: "${refinedSinglePlayer.guid}". This may indicate an incomplete or corrupted player record.`);
+                      } else {
+                        refinedSinglePlayer.appearance = {};
+                        const appearanceKeys = [
+                          'BodyMeshName',
+                          'HeadMeshName',
+                          'HairMeshName',
+                          'HairColor',
+                          'BrowColor',
+                          'BodyColor',
+                          'BodySubsurfaceColor',
+                          'EyeColor',
+                          'EyeMaterialName',
+                          'VoiceID'
+                        ]
+                        for (const thisAppearanceKey of appearanceKeys) {
+                          if (playerFileNestedData?.PlayerCharacterMakeData?.value?.[`${thisAppearanceKey}`]?.value) {
+                            try {
+                              const thisCurrentValue = playerFileNestedData?.PlayerCharacterMakeData?.value?.[`${thisAppearanceKey}`]?.value;
+                              if (thisCurrentValue) {
+                                if (typeof thisCurrentValue === 'string') {
+                                  refinedSinglePlayer.appearance[`${thisAppearanceKey}`] = thisCurrentValue;
+                                } else {
+                                  refinedSinglePlayer.appearance[`${thisAppearanceKey}`] = JSON.stringify(thisCurrentValue);
+                                }
+                              } else {
+                                refinedSinglePlayer.appearance[`${thisAppearanceKey}`] = null;
+                              }
+                            } catch (err) {
+                              // ignore for now
+                              console.error(`Error stringifying appearance key ${thisAppearanceKey}: ${err.message}`)
+                            }
+                          } else {
+                            console.info('Unable to find key', `playerFileNestedData?.PlayerCharacterMakeData?.value?.[${thisAppearanceKey}]?.value`)
+                          }
+                        }
+                      }
+                    }
+
+                    const playerRecordData = playerFileNestedData?.RecordData?.value || null;
+                    if (playerRecordData) {
+                      refinedSinglePlayer.countTribesDefeated = playerRecordData?.TribeCaptureCount?.value || 0;
+                      refinedSinglePlayer.countEffigiesFound = playerRecordData?.RelicPossessNum?.value || 0;
+
+                      // Some quick totals
+                      refinedSinglePlayer.countTotalPetCaptures = playerRecordData?.PalCaptureCount?.value.reduce((accumulator, current) => (accumulator + current.value), 0) || 0;
+                      refinedSinglePlayer.countTotalUnlockedFastTravels = playerRecordData?.FastTravelPointUnlockFlag?.value?.length || 0;
+                      refinedSinglePlayer.countTotalNotesFound = playerRecordData?.NoteObtainForInstanceFlag?.value?.length || 0;
+
+                      // List captured Pals
+                      if (appConfig?.reporting?.showCapturedPals !== false) {
+                        refinedSinglePlayer.palsCaptured = playerRecordData?.PalCaptureCount?.value || [];
+                      }
+                    }
+                  }
+
+                } else {
+                  errors.push(`Error loading ${refinedSinglePlayer.handle}'s converted save file into memory`);
+                }
+
+              } catch (err) {
+                errors.push(`Error loading ${refinedSinglePlayer.handle}'s converted save file into memory, see logs.`)
+                console.error(`Error loading ${refinedSinglePlayer.handle}'s converted save file into memory: ${err.message}`);
+              }
+            }
+            
+          } catch (err) {
+            console.error(`Error converting save file for player: ${refinedSinglePlayer.handle || refinedSinglePlayer.guid}`, err)
+            errors.push(`Error converting save file for player: ${refinedSinglePlayer.handle || refinedSinglePlayer.guid}`)
+          }
+
+          // Add the player to our refined player map
+          refinedPlayerMap.push(refinedSinglePlayer);
+          iteratedPlayerIndex++; // Bump our index
+
+        } else {
+          errors.push(`Unable to find nested player data (handle, level, etc) for player with GUID: "${refinedSinglePlayer.guid}". This may indicate an incomplete or corrupted player record.`)
+        }
+
+        reportData.players = refinedPlayerMap;
+      }
+
+      if (appConfig?.reporting?.showPlayerData !== false) {
+        console.info('\n\n===========================')
+        console.info(`Data on all of your current players`, refinedPlayerMap);
+        console.info('===========================')
+
+        console.info(`Pro Tip: Almost any values you see above (except fields with "total" in the name) can be modified! Just add them to the "changesToMake" section in your config file!\n\n`)
+      }
+    }
+    
+    /**
+     * If we are this far, we're now ready to start attempting to make changes.
+     */
     const { changesToMake } = appConfig;
     if (!changesToMake) {
-      console.info(`Your config file doesn't list any changes to make, so none will be attempted.`)
-      return;
+      console.info(`Your config file doesn't list any changes to make, so none will be attempted. The app will only generate a report.`);
     }
+
+    /**
+     * Export report as needed or if reporting isn't specified
+     */
+    if (appConfig?.reporting?.export !== false) {
+      console.info("Exporting report as JSON...")
+      try {
+
+        // Add generation timestmap to report
+        reportData.summary.reportStartedAt = appStartedTimestamp;
+        reportData.summary.reportExportedAt = new Date().toISOString();
+
+        let outputFilePath = `./reports/${filenameCompatibleTimestamp()}.json`;
+        // If we have a path given, use that instead
+        if (appConfig?.reporting?.exportPath && appConfig?.reporting?.exportPath?.endsWith('.json')) {
+          console.info("exportPath was given and looks like a filename, so we'll use that.")
+          outputFilePath = appConfig?.reporting?.exportPath;
+        } else if (appConfig?.reporting?.exportPath) {
+          // Treat it as a directory
+          console.info('exportPath was given without a .json extension, so we will treat it as a directory and use a timestamp.json filename.')
+          outputFilePath = `${appConfig?.reporting?.exportPath}/${filenameCompatibleTimestamp()}.json`;
+        }
+
+        const exportReportDir = path.dirname(outputFilePath);
+        fs.mkdirSync(exportReportDir, { recursive: true });
+
+        const finalReportData = {
+          ...reportData,
+        }
+
+        // Just easier to do this check here than earlier
+        // Exclude player data if we've explicitly declared showPlayerData as false
+        if (appConfig?.reporting?.showPlayerData === false) {
+          finalReportData.players = undefined;
+        }
+
+        fs.writeFileSync(outputFilePath, JSON.stringify(finalReportData, null, 2), 'utf8');
+        console.log(`Report written written to ${outputFilePath}`);
+      } catch (error) {
+        console.error(`Error writing to file: ${error.message}`);
+      }
+    }
+
+    // TODO: Iterate over each of our changes and apply based on what we need.
   }
 
   if (errors.length > 0) {
+    console.info('\n\n===========================')
     console.error('One or more errors were found during run:', errors)
+    console.info('===========================\n\n')
   }
+
+  console.info('\n\n===========================')
+  console.info('Application has finished running. Scroll up for logs (we will export this to a file in a future update)')
 }
 
 // Rawr
